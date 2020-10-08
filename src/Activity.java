2d4b2ca7ff5a9ada82574203c0d343a9d68ebfbf
alesslow <alesslow@web.de>
2020-10-08 18:08:36 +0200

    added am/pm fixation

--- src/Activity.java
+++ src/Activity.java
@@ -1,24 +1,23 @@
 import java.util.*;
 import java.io.*;
 
-public class Activity {
+public class Activity implements Serializable {
     Scanner setActivityName = new Scanner(System.in);
     Scanner setActivityLocation = new Scanner(System.in);
     Scanner setActivityDate = new Scanner(System.in);
     Scanner setActivityDuration = new Scanner(System.in);
-    Scanner setActivityAmPm = new Scanner(System.in);
 
     ArrayList ToDoList;
     String name;
     String location;
     String dateDay ;
-    String AmPm = " am/pm";
+    String AmPm;
     int timeOfDay;
     int duration;
     int end;
 
 
-    public void setActivity() {
+    public void createActivity() {
 
         ToDoList = new ArrayList();
         System.out.println("What kind of Activity would you like to add to the List?");
@@ -34,18 +33,19 @@ public class Activity {
         ToDoList.add(location);
         System.out.print("'" + name + "' " + "will take place at " + "'" + location + "'");
         System.out.println();
-
         System.out.println();
 
         System.out.println("At what date and time?");
         dateDay = setActivityDate.nextLine();
         timeOfDay = setActivityDate.nextInt();
+        AmPm = setActivityDate.next();
         ToDoList.add(dateDay);
         ToDoList.add(timeOfDay);
+        ToDoList.add(AmPm);
         System.out.println();
         System.out.println("The date and time of " + "'" + name + "'" + " at " + "'" + location + "'" + " are");
         System.out.println(dateDay);
-        System.out.println(timeOfDay + "am/pm");
+        System.out.println(timeOfDay + AmPm);
 
 
         System.out.println();
@@ -54,20 +54,30 @@ public class Activity {
         duration = setActivityDuration.nextInt();
         ToDoList.add(duration);
         System.out.println(name + " at " + location + " on the " +
-                        dateDay + " at " + timeOfDay + AmPm + " will take " +
+                        dateDay + " at " + timeOfDay + AmPm + " and will take " +
                         duration + " hours");
         end = duration + timeOfDay;
 
 
         System.out.println();
-        System.out.println();
         if (ToDoList.contains(duration)){
+
             System.out.println(name);
             System.out.println(location);
             System.out.println(dateDay);
             System.out.println(timeOfDay + AmPm);
             System.out.println(duration + " hour duration");
-            System.out.println("ends at " + end);
+            
+            if (AmPm.contains("am")){
+            System.out.println("ends at " + end + "am");
+            } else {
+                System.out.println("ends at " + end + "pm");
+            }
+
+            System.out.println();
+            System.out.println();
+            System.out.println();
+
 
 
         }
--- src/ToDoLaunch.java
+++ src/ToDoLaunch.java
@@ -6,8 +6,15 @@ public class ToDoLaunch {
 
     public static void main(String[]args) {
 
-         Activity activity = new Activity();
-         activity.setActivity();
+         Activity activity1 = new Activity();
+         activity1.createActivity();
+
+         Activity activity2 = new Activity();
+         activity2.createActivity();
+
+         System.out.println(activity1.ToDoList);
+         System.out.println();
+         System.out.println(activity2.ToDoList);
 
     }
 }
